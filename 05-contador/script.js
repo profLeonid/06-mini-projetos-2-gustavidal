62d12e5b2f4ccc5032c2e3aa3a204b8d95012e06
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-03-24 15:33:11 -0300

    feat: finalizar projeto

--- 05-contador/script.js
+++ 05-contador/script.js
@@ -51,26 +51,38 @@ function criarListaPotencia2(quantidade) {
 
 function criarLinha(num, par, impar, mult5, pot2) {
     const tbody = document.getElementById('tbody')
-    const tr = document.createElement('tr')
+    const tr    = document.createElement('tr')
 
-    const tdNum = document.createElement('td')
-    tdNum.textContent = num
-
-    const tdPar = document.createElement('td')
-    tdPar.textContent = par
-
-    const tdImpar = document.createElement('td')
-    tdImpar.textContent = impar
-
+    const tdNum    = document.createElement('td')
+    const tdPar    = document.createElement('td')
+    const tdImpar  = document.createElement('td')
     const tdMulti5 = document.createElement('td')
-    tdMulti5.textContent = mult5
+    const tdPot2   = document.createElement('td')
 
-    const tdPot2 = document.createElement('td')
-    tdPot2.textContent = pot2
+    tdNum.textContent    = num
+    tdPar.textContent    = par
+    tdImpar.textContent  = impar
+    tdMulti5.textContent = mult5
+    tdPot2.textContent   = pot2
 
     tr.replaceChildren(tdNum, tdPar, tdImpar, tdMulti5, tdPot2)
 
     tbody.appendChild(tr)
 }
 
-criarLinha(10, 20, 10, 50, 10)
+// FUNCIONAMENTO
+
+function handleClick() {
+    const quantidade = Number(document.getElementById('quantidade').value)
+
+    const listaNumeros = criarListaNumeros(quantidade)
+    const listaPares   = criarListaPares(quantidade)
+    const listaImpares = criarListaImpares(quantidade)
+    const listaMulti5  = criarListaMulti5(quantidade)
+    const listaPot2    = criarListaPotencia2(quantidade)
+
+    document.getElementById('tbody').replaceChildren()
+
+    for (let i = 0; i < quantidade; i++)
+        criarLinha(listaNumeros[i], listaPares[i], listaImpares[i], listaMulti5[i], listaPot2[i])
+}
